999c1ee19cab8bca93b6606d72829679ab11cee2
Tiz <dannilo@bk.ru>
2024-09-27 10:12:42 +0400

    Task2 Compl

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -10,15 +10,30 @@
             while (!Directory.Exists(path)) {
                 Console.WriteLine("Введите путь дирректории");
                 path = Console.ReadLine();
-                
             }
 
+            long size = DirSize(new System.IO.DirectoryInfo(path));
 
+            Console.WriteLine($"Вес дирректории:{size} байт");
 
+            static long DirSize(DirectoryInfo d)
+            {
+                long Size = 0;
+               
+                FileInfo[] fis = d.GetFiles();
+                foreach (FileInfo fi in fis)
+                {
+                    Size += fi.Length;
+                }
 
+                DirectoryInfo[] dis = d.GetDirectories();
 
-
-
+                foreach (DirectoryInfo di in dis)
+                {
+                    Size += DirSize(di);
+                }
+                return (Size);
+            }
         }
     }
 }
